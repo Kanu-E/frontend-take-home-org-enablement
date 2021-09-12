7f091ccc24515f7eb9427fbff5a6d38e13cf2056
kanu-e <chukwuemekakanu@gmail.com>
2021-09-11 22:47:51 -0500

    Fetch events

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React, {Component} from "react"
 class App extends Component{
 
 
-
+  componentDidMount(){
+    fetch('/events')
+    .then(res => {
+      return res.json()
+   }).then(res => {
+     console.log(res)
+     ;
+   })
+  }
   
   
   render(){
